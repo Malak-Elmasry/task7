6f320551735015ff570825bff1d99852036eb482
Malak-Elmasry <malak20192004gmail.com>
2023-08-10 20:21:12 +0300

    first commit

---
+++ task_7.py
@@ -0,0 +1,55 @@
+# Q1
+# row = int(input('Enter the number'))
+# for i in range (1, row+1):
+#     for j in range(1, i + 1):
+#               print(j, end = '')
+#     print('')
+
+
+# Q2
+# num = int(input('Enter the name'))
+# sum = 0
+# for x in range (1,num+1):
+#     sum+=x
+# print(sum)
+
+
+# Q3
+# name = input('Enter the name')
+# for x in range(len(name)):
+#     print(name[:x])
+#
+# for i in range(len(name)+1):
+#     print(name[:len(name)-i])
+
+
+
+# Q4
+# name =input('Enter the name')
+# print(name[::-1])
+
+# 5
+# num = int(input('Enter the number'))
+# k=1
+# while num >= k:
+#     print(num, '', end = '')
+#     num -= 1
+
+
+# 6
+# for x in range (1, 11):
+#     print(x*5)
+
+
+# 7
+# num1 = int(input('Enter the Limit_number:'))
+# num2 = int(input('Enter the Target_number: '))
+# num3 = int(input('Enter the number'))
+# for x in range(1,num1):
+#  if x <= num2:
+#   print(x*num3)
+
+
+
+
+
